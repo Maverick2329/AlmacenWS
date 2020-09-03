ff5d8de1a7cf3763e27b69dd2505b10f561a4476
Maverick2329 <ing.tomas.imartinez@outlook.com>
2020-09-02 23:20:36 -0500

    Se agrega el controlador de categoria y su viemodel

---
+++ AlmacenWS/Controllers/CategoriaController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AlmacenWS.Models;
+using AlmacenWS.Models.Response;
+using AlmacenWS.Models.ViewModels;
+
+namespace AlmacenWS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Respuesta respuesta = new Respuesta();
+            try
+            {
+                using (Almacen_dbContext db = new Almacen_dbContext())
+                {
+                    var listaCategorias = db.Categoria.OrderByDescending(t => t.IdCategoria).ToList();
+                    respuesta.Exito = 1;
+                    respuesta.Data = listaCategorias;
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return Ok(respuesta);
+        }
+
+        [HttpPost]
+        public IActionResult Add(CategoriaViewModel Categoria)
+        {
+            Respuesta respuesta = new Respuesta();
+            try
+            {
+                using (Almacen_dbContext db = new Almacen_dbContext())
+                {
+                    Categoria categoria = new Categoria();
+                    categoria.NombreCategoria = Categoria.NombreCategoria;
+                    categoria.DescripcionCategoria = Categoria.DescripcionCategoria;
+                    db.Categoria.Add(categoria);
+                    db.SaveChanges();
+                    respuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return Ok(respuesta);
+        }
+
+        [HttpPut]
+        public IActionResult Edit(CategoriaViewModel Categoria)
+        {
+            Respuesta respuesta = new Respuesta();
+            try
+            {
+                using(Almacen_dbContext db = new Almacen_dbContext())
+                {
+                    Categoria categoria = db.Categoria.Find(Categoria.IdCategoria);
+                    categoria.NombreCategoria = Categoria.NombreCategoria;
+                    categoria.DescripcionCategoria = Categoria.DescripcionCategoria;
+                    db.Entry(categoria).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    respuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return Ok(respuesta);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int Id)
+        {
+            Respuesta respuesta = new Respuesta();
+            try
+            {
+                using (Almacen_dbContext db = new Almacen_dbContext())
+                {
+                    Categoria categoria = db.Categoria.Find(Id);
+                    db.Remove(categoria);
+                    db.SaveChanges();
+                    respuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return Ok(respuesta);
+        }
+    }
+}
---
+++ AlmacenWS/Models/ViewModels/CategoriaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlmacenWS.Models.ViewModels
+{
+    public class CategoriaViewModel
+    {
+        public int IdCategoria { get; set; }
+        public string NombreCategoria { get; set; }
+        public string DescripcionCategoria { get; set; }
+    }
+}
